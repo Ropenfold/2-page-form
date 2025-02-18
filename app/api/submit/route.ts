7fc0e1efb222976b8f14c09b195d37bb2aae0348
Stuart Roper <stuart.roper@bravissimo.com>
2025-02-18 22:04:31 +0000

    add route for form post

---
+++ app/api/submit/route.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(req: NextRequest) {
+  try {
+    const formData = await req.formData();
+    const data = Object.fromEntries(formData);
+
+    console.log('Recieved form data:', data);
+
+    const paramaters = new URLSearchParams(
+      Object.fromEntries(
+        Object.entries(data).map(([key, value]) => [key, String(value)])
+      )
+    ).toString();
+
+    return NextResponse.redirect(new URL(`/details?${paramaters}`, req.url));
+  } catch (error) {
+    console.error('Error handling POST request', error);
+    return NextResponse.json({ error: 'internal Server Error' }, { status: 500});
+  }
+}
