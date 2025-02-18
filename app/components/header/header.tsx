61c991f8dc0ca310070512beb84862da82b9fc40
Stuart Roper <stuart.roper@bravissimo.com>
2025-02-18 22:06:46 +0000

    create header component

---
+++ app/components/header/header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './header.module.css';
+
+const Header = () => {
+  return (
+    <div className={styles.headerContainer}>
+        <h1 className={styles.h1}>CheckaCar.com</h1>
+    </div>
+    
+  )
+}
+
+export default Header;
